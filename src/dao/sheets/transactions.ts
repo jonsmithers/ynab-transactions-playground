f7cbab7b4d248e522caff78abf25fcb3871f975f
Billy Hardy <billyhardy@google.com>
2020-03-29 20:54:38 -0400

    Add transactions service that targets sheets.

---
+++ src/dao/sheets/transactions.ts
@@ -0,0 +1,47 @@
+import {sheets_v4} from 'googleapis';
+
+import {Transaction} from '../../beans/transaction';
+import {TransactionsDAO} from '../interface/transactions';
+import {SheetRangeBuilder} from './interfaces';
+
+
+export class SheetsTransactionsDAO implements TransactionsDAO {
+  constructor(
+      readonly sheetsService: sheets_v4.Sheets,
+      readonly sheetRangeBuilder: SheetRangeBuilder) {
+    this.sheetRangeBuilder.withSheetPrefix('Transactions: ');
+  }
+
+  getAllInBudget(b_id: string): Promise<Transaction[]> {
+    return this.sheetsService.spreadsheets.values
+        .get(this.sheetRangeBuilder.withSheet(b_id).build())
+        .then((val) => {
+          return val.data.values!.map(
+              (row) => Transaction.fromSheetsArray(row));
+        });
+  }
+
+  getById(): Promise<Transaction> {
+    throw new Error('Not implemented, try using cached service.');
+  }
+
+  save(b_id: string, transaction: Transaction): Promise<Transaction> {
+    return this.sheetsService.spreadsheets.values
+        .append({
+          ...this.sheetRangeBuilder.withSheet(b_id).build(),
+          valueInputOption: 'USER_ENTERED',
+          requestBody: {
+            values: [transaction.toSheetsArray(b_id)],
+          }
+        })
+        .then(() => transaction);
+  }
+
+  update(): Promise<Transaction> {
+    throw new Error('Not implemented');
+  }
+
+  delete(): Promise<Transaction> {
+    throw new Error('Not implemented');
+  }
+}
