f1c161eda837b48c0d08040ef3c1c9db8f897a5c
Billy Hardy <billyhardy@google.com>
2020-03-29 20:52:48 -0400

    Clean up transaction and add methods to translate to/from sheets.

--- src/beans/transaction.ts
+++ src/beans/transaction.ts
@@ -1,59 +1,51 @@
-import {SaveTransaction, TransactionDetail} from 'ynab';
+import {SaveTransaction, SubTransaction, TransactionDetail} from 'ynab';
+
+import {fromNullable} from '../util/option';
+
+export interface TransactionData {
+  id: string;
+  account_id: string;
+  account_name: string;
+  date: string;
+  amount: number;
+  payee_id?: string|null;
+  payee_name?: string|null;
+  category_id?: string|null;
+  category_name?: string|null;
+  memo?: string|null;
+  cleared: TransactionDetail.ClearedEnum;
+  approved: boolean;
+  flag_color?: TransactionDetail.FlagColorEnum|null;
+  import_id?: string|null;
+  subtransactions: SubTransaction[];
+}
 
 export class Transaction {
-  constructor(readonly transaction: TransactionDetail) {}
-
-  static getTestHeader(): string {
-    return `
-      deleted,
-      cleared,
-      approved,
-      date,
-      amount,
-      category_name,
-      account_name,
-      payee_name,
-      memo,
-    `;
-  }
-
-  toTestCSV(): string {
-    return `
-      ${this.transaction.deleted},
-      ${this.transaction.cleared},
-      ${this.transaction.approved},
-      ${this.transaction.date},
-      ${this.transaction.amount / 1000},
-      ${this.transaction.account_name},
-      ${this.transaction.category_name},
-      ${this.transaction.payee_name},
-      ${this.transaction.memo},
-    `;
-  }
-
-  static getHeader(): string {
-    return 'deleted,cleared,approved,date,amount,category_name,account_name,payee_name,memo';
-  }
-
-  toCSV(): string {
-    return `${this.transaction.deleted},${this.transaction.cleared},${
-        this.transaction.approved},${this.transaction.date},${
-        this.transaction.amount / 1000},${this.transaction.category_name},${
-        this.transaction.account_name},${this.transaction.payee_name},${
-        this.transaction.memo}`;
-  }
+  constructor(readonly transaction: TransactionData) {}
 
   get id(): string {
     return this.transaction.id;
   }
 
+  get date(): string {
+    return this.transaction.date;
+  }
+
+  get amount(): number {
+    return this.transaction.amount;
+  };
+
   get inflow(): number {
-    return Math.abs(Math.max(0, this.transaction.amount));
+    return Math.abs(Math.max(0, this.amount));
   };
 
   get outflow(): number {
-    return Math.abs(Math.min(0, this.transaction.amount));
+    return Math.abs(Math.min(0, this.amount));
   };
+
+  get category_id(): string {
+    return this.transaction.category_id ?? '';
+  }
 
   get category_name(): string {
     if (this.transaction.category_name === 'Immediate Income SubCategory') {
@@ -62,28 +54,104 @@ export class Transaction {
     return this.transaction.category_name ?? '';
   }
 
+  get account_name(): string {
+    return this.transaction.account_name;
+  }
+
+  get account_id(): string {
+    return this.transaction.account_id;
+  }
+
+  get memo(): string {
+    return fromNullable(this.transaction.memo).unwrapOr('');
+  }
+
+  get cleared(): string {
+    return this.transaction.cleared.toString();
+  }
+
+  get approved(): boolean {
+    return this.transaction.approved;
+  }
+
+  get payee_id(): string {
+    return fromNullable(this.transaction.payee_id).unwrapOr('');
+  }
+
+  get payee_name(): string {
+    return fromNullable(this.transaction.payee_name).unwrapOr('');
+  }
+
+  get flag_color(): string {
+    return fromNullable(this.transaction.flag_color)
+        .map(f => f.toString())
+        .unwrapOr('');
+  }
+
+  get import_id(): string {
+    return this.transaction.import_id ?? '';
+  }
+
   toAspire(): string {
-    return `${this.transaction.date},${this.inflow / 1000},${
-        this.outflow /
-        1000},${this.category_name},${this.transaction.account_name},${
-        this.transaction.memo},${this.transaction.cleared},${
-        this.transaction.approved},${this.transaction.payee_name}`;
+    return `${this.date},${this.inflow / 1000},${this.outflow / 1000},${
+        this.category_name},${this.account_name},${this.memo},${this.cleared},${
+        this.approved},${this.payee_name}`;
   }
 
   toSaveObject(): SaveTransaction {
     return {
-      account_id: this.transaction.account_id,
-      date: this.transaction.date,
-      amount: this.transaction.amount,
+      account_id: this.account_id,
+      date: this.date,
+      amount: this.amount,
       payee_id: this.transaction.payee_id,
-      payee_name: this.transaction.payee_name,
+      payee_name: this.payee_name,
       category_id: this.transaction.category_id,
-      memo: this.transaction.memo,
+      memo: this.memo,
       cleared: this.transaction.cleared,
-      approved: this.transaction.approved,
+      approved: this.approved,
       flag_color: this.transaction.flag_color,
       import_id: this.transaction.import_id,
       subtransactions: this.transaction.subtransactions,
     };
+  }
+
+  static fromSheetsArray(row: any[]): Transaction {
+    return new Transaction({
+      id: row[1],
+      account_id: row[2],
+      account_name: row[3],
+      date: row[4],
+      amount: row[5],
+      payee_id: row[6],
+      payee_name: row[7],
+      category_id: row[8],
+      category_name: row[9],
+      memo: row[10],
+      cleared: row[11],
+      approved: row[12],
+      flag_color: row[13],
+      import_id: row[14],
+      subtransactions: [],
+    });
+  }
+
+  toSheetsArray(b_id: string): any[] {
+    return [
+      b_id,
+      this.id,
+      this.account_id,
+      this.account_name,
+      this.date,
+      this.amount,
+      this.payee_id,
+      this.payee_name,
+      this.category_id,
+      this.category_name,
+      this.memo,
+      this.cleared,
+      this.approved,
+      this.flag_color,
+      this.import_id,
+    ];
   }
 }
