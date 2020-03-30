8de28ee5df1423ffd8dcc511448886d3a63b9001
Billy Hardy <billyhardy@google.com>
2020-03-29 20:53:45 -0400

    Add interface for sheets range and add a builder for it.

---
+++ src/dao/sheets/interfaces.ts
@@ -0,0 +1,31 @@
+export interface SheetRange {
+  range: string;
+  spreadsheetId: string;
+}
+
+export class SheetRangeBuilder {
+  private sheetPrefix: string = '';
+  private sheet: string = '';
+
+  constructor(private range: string, private readonly spreadsheetId: string) {}
+
+  withSheetPrefix(sheetPrefix: string): SheetRangeBuilder {
+    this.sheetPrefix = sheetPrefix;
+    return this;
+  }
+
+  withSheet(sheet: string): SheetRangeBuilder {
+    this.sheet;
+    this.sheetPrefix;
+    this.sheet = sheet;
+    return this;
+  }
+
+  build(): SheetRange {
+    return {
+      range: this.range,
+      // range: `'${this.sheetPrefix}${this.sheet}'!${this.range}`,
+      spreadsheetId: this.spreadsheetId
+    };
+  }
+}
